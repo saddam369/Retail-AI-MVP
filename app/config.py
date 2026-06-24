20463616ddd0fd15874510a9181c42af297412d8
MD Saddam <mdsaddam@Mac.lan>
2026-06-24 16:06:37 +0530

    DB url change

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/retail"
+# DATABASE_URL = "postgresql://postgres:root@localhost:5432/retail"
+DATABASE_URL = "postgresql://postgres:DTjdNHfnyjmBiLHqUXExtdKCiZhkEwjR@reseau.proxy.rlwy.net:49454/retail"
 
 OLLAMA_URL = "http://localhost:11434"
 
